cd50b6c8b98d4a466602939a1b2f4260c41a3e57
Scott Bullock <sjb8108@g.rit.edu>
2023-02-15 19:33:53 -0500

    Add.java finished

---
+++ src/machine/instructions/Add.java
@@ -0,0 +1,22 @@
+package machine.instructions;
+
+import machine.InstructionStack;
+import machine.Maquina;
+
+public class Add implements Instruction{
+    private final InstructionStack stack;
+    public Add(Maquina machine){
+        this.stack = machine.getInstructionStack();
+    }
+    @Override
+    public void execute(){
+        int firstpop = this.stack.pop();
+        int secondpop = this.stack.pop();
+        int added = firstpop+secondpop;
+        this.stack.push(added);
+    }
+    @Override
+    public String toString() {
+        return Maquina.ADD;
+    }
+}
